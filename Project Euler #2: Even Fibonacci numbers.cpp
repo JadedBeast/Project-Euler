c373886c148b6d5f39919b7684d0eb8f1ffbedac
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2019-04-28 21:30:09 +0200

    Create Project Euler #2: Even Fibonacci numbers.cpp

---
+++ Project Euler #2: Even Fibonacci numbers.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+    int t ;
+    long long sum ,f0,f1,z,n;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%lld",&n);
+        f0 = 1,f1=1;
+        sum = 0,z=0;
+
+        while (z<n) {
+            z = f0+f1;
+            if (z%2==0 && z<n)
+                sum+= z;
+
+            f0 = f1;
+            f1 = z;
+        }
+    printf("%lld\n",sum);
+    }   
+}
+
